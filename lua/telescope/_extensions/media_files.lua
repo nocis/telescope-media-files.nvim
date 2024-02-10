c6d4eb332607eb50014b13afc18c8f3feb5b9d5c
Jared Yang <nociszy@gmail.com>
2024-02-10 04:38:24 -0800

    Update media_files.lua

--- lua/telescope/_extensions/media_files.lua
+++ lua/telescope/_extensions/media_files.lua
@@ -43,7 +43,7 @@ M.media_preview = defaulter(function(opts)
       end)()
         
       local et =  vim.filetype.match({ filename = vim.fn.expand(filename) })
-      print(et,filename)
+      print(et,vim.fn.expand(filename))
       local fset = {"png", "jpg", "gif", "mp4", "webm", "pdf"}
       if fset[et] == nil then
           if get_file_stat(filename).type == "directory" then
